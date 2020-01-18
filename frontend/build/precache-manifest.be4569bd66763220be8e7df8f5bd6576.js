ae3c06c17b7856be50ba67217db2103349eaa3d2
Fabiano Couto <facodeli@gmail.com>
2020-01-18 18:15:32 -0300

    frontend build

---
+++ frontend/build/precache-manifest.be4569bd66763220be8e7df8f5bd6576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8e5c4b5ef22ffac12c40eed341b67a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f4ebe1f9619ccbae4215",
+    "url": "./static/css/main.495397c4.chunk.css"
+  },
+  {
+    "revision": "366c5cf3ddad7f7de738",
+    "url": "./static/js/2.c4722873.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.c4722873.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4ebe1f9619ccbae4215",
+    "url": "./static/js/main.0f867a6d.chunk.js"
+  },
+  {
+    "revision": "2c6d7ef4598b0c12856f",
+    "url": "./static/js/runtime-main.2e928bae.js"
+  }
+]);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 // https://devfinderapi.herokuapp.com/
+// http://localhost:3333
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://devfinderapi.herokuapp.com'
 });
 
 export default api;
